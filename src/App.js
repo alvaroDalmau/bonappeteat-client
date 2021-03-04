71d5ad0c1bed81ae1acc06eda2106c1316690ef5
Alvaro Dalmau <dalmau.alvaro@gmail.com>
2021-03-04 11:48:26 +0100

    solving errors in merge with nora-code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import NavBar from './components/NavBar';
+// import NavBar from './components/NavBar';
 
 export default class App extends Component {
   render() {
     return <React.Fragment>
-      <NavBar />
+      {/* <NavBar /> */}
       <Switch>
         <Route patch='/'/>
         <Route patch='/restaurant'/>

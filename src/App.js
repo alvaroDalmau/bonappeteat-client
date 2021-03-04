175da0087ce01c1b893d8eb625a3c0e863b4eca5
Nora Martin <noramartiin96@gmail.com>
2021-03-04 12:42:29 +0100

    starting routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
-// import NavBar from './components/NavBar';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import AboutUs from './components/AboutUs'
 
  class App extends Component {
   render() {
     return <React.Fragment>
-      {/* <NavBar /> */}
+      <NavBar />
       <Switch>
-        <Route patch='/'/>
+        <Route exact patch='/' render={(routerProps)=>{
+          return(
+            <NavBar {...routerProps}/>,
+            <Home {...routerProps}/>,
+            <AboutUs {...routerProps}/>
+          )
+        }}/>
         <Route patch='/restaurant'/>
         <Route patch='/profile'/>
       </Switch>
---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class NavBar extends Component {
+    render() {
+        return (
+            <div>
+                <h1>NavBar</h1>
+            </div>
+        )
+    }
+}
+export default NavBar

8da89fcf4f52f325f438f8d88f176842ce719490
Alvaro Dalmau <dalmau.alvaro@gmail.com>
2021-03-05 17:35:04 +0100

    fixing cookies on home

--- src/components/LogForm.js
+++ src/components/LogForm.js
@@ -4,29 +4,17 @@ export default class LogForm extends Component {
   render() {
     const { onSign } = this.props;
     return (
-      <div>
-        <form onSubmit={onSign}>
-          <div>
-          {/* DEPENDE */}
-            <form onSubmit={this.props.onSign}>
-              <div>
-                <label htmlFor="InputEmail">Email address</label>
-                <input type="email" name="email" />
-              </div>
-              <div>
-                <label htmlFor="InputPassword">Password</label>
-                <input name="password" type="password" />
-              </div>
-              <button type="submit">Submit</button>
-            </form>
-          </div>
-          <div>
-            <label htmlFor="InputPassword">Password</label>
-            <input name="password" type="password" />
-          </div>
+      <React.Fragment>
+        <form id="form" onSubmit={onSign}>
+          <label>Email address</label>
+          <input type="email" name="email" />
+
+          <label>Password</label>
+          <input name="password" type="password" />
+
           <button type="submit">Submit</button>
         </form>
-      </div>
+      </React.Fragment>
     );
   }
 }
--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -27,55 +27,56 @@ class Restaurant extends Component {
 
   handleSearch = event => {
     let searchText = event.target.value.toLowerCase();
-      console.log(this.state.filterRestaurant);
-    let filteredArray = this.state.restaurants.filter((singleRestaurant) => {
+    console.log(this.state.filterRestaurant);
+    let filteredArray = this.state.restaurants.filter(singleRestaurant => {
       return singleRestaurant.name.toLowerCase().includes(searchText);
     });
 
-    let y=[]
-    this.state.filterRestaurant.forEach((e)=>{filteredArray.forEach((j)=>{
-     if ( e.name == j.name){y.push(j)}
-    })})
+    let y = [];
+    this.state.filterRestaurant.forEach(e => {
+      filteredArray.forEach(j => {
+        if (e.name == j.name) {
+          y.push(j);
+        }
+      });
+    });
 
-    console.log(y)
+    console.log(y);
     this.setState({
       filterRestaurant: filteredArray,
     });
   };
 
-  handleFilter = (event) => {
-
+  handleFilter = event => {
     let filterKey = event.target.value;
 
-    if(filterKey == 'all'){
+    if (filterKey == 'all') {
       this.setState({
         filterRestaurant: this.state.restaurants,
       });
-    }
-    else{
-    let filteredArray = this.state.restaurants.filter((singleRestaurant) => {
-      return singleRestaurant.category.includes(filterKey);
-      
-    });
+    } else {
+      let filteredArray = this.state.restaurants.filter(singleRestaurant => {
+        return singleRestaurant.category.includes(filterKey);
+      });
 
-    this.setState({
-      filterRestaurant: filteredArray,
-    });
+      this.setState({
+        filterRestaurant: filteredArray,
+      });
     }
   };
 
   render() {
-    // const { restaurants, filterRestaurant } = this.state;
+    const { restaurants, filterRestaurant } = this.state;
     return (
       <div>
         <h1>All the restaurants</h1>
         {/* FILTER FORM */}
 
         <select onChange={this.handleFilter}>
-          <option key={restaurants._id} value='all'>
-           All
+          <option key={restaurants._id} value="all">
+            All
           </option>
-          {restaurants.map((restaurants) => (
+          {restaurants.map(restaurants => (
             <option key={restaurants._id} value={restaurants.category}>
               {restaurants.category}
             </option>
@@ -92,7 +93,7 @@ class Restaurant extends Component {
               <Link key={restaurants._id} to={`/restaurant/${restaurants._id}`}>
                 {restaurants.name}
               </Link>
-              <div> {restaurant.location}</div>
+              <div> {restaurants.location}</div>
             </div>
           );
         })}
